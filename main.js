c57407933d541ba7971a683094cd2095f8c04ce3
Matthew Kaufer <matthew@kaufer.org>
2014-10-11 20:08:43 -0400

    Comment posting is a go!

--- main.js
+++ main.js
@@ -1,88 +1,79 @@
-var request  = require('request')
-  , argv     = require('yargs').argv
-  , modhash
-  , cookie;
+var request	= require('request')
+	, argv	= require('yargs').argv
+	, modhash
+	, cookie;
 
 subredditName = "Mongit";
 
+var loggedIn = false;
 
 
 function login (callback) {
-  var options = {
-      url     : 'https://ssl.reddit.com/api/login?api_type=json&user=' + argv.user + '&passwd=' + argv.pass + '&rem=True',
-      headers : {
-        'User-Agent' : 'Mongit/0.0.1 by mjkaufer',
-      },
-      method  : 'POST'
-  };
+	var options = {
+			url	: 'https://ssl.reddit.com/api/login?api_type=json&user=' + argv.user + '&passwd=' + argv.pass + '&rem=True',
+			headers : {
+				'User-Agent' : 'Mongit/0.0.1 by mjkaufer',
+			},
+			method : 'POST'
+	};
 
-  request(options, function (err, res, body) {
-    if (err) {
-      console.log('LOGIN ERROR:');
-      console.log(err);
-      return;
-    } else {
-      var parsedBody = JSON.parse(body);
-      modhash = parsedBody.json.data.modhash;
-      cookie  = parsedBody.json.data.cookie;
-      console.log("WE LOGGED IN");
-      callback();
-    }
-  });
+	request(options, function (err, res, body) {
+		if (err) {
+			console.log('LOGIN ERROR:');
+			console.log(err);
+			return;
+		} else {
+			var parsedBody = JSON.parse(body);
+			modhash = parsedBody.json.data.modhash;
+			cookie	= parsedBody.json.data.cookie;
+			console.log("WE LOGGED IN");
+			loggedIn = true;
+			return callback();
+		}
+	});
 }
-
-// function postText (message) {//this won't work bc we need to solve captchas :'(
-//   var text     = message
-//     , options  = {
-//         url      : 'https://en.reddit.com/api/submit?api_type=json&sendreplies=false&title=DB' + new Date().getTime() + '&text=' + encodeURIComponent(text) + '&kind=self&sr=' + subredditName,
-//         headers  : {
-//             'User-Agent' : 'Mongit/0.0.1 by mjkaufer',
-//             'X-Modhash'  : modhash,
-//             'Cookie'     : 'reddit_session=' + encodeURIComponent(cookie)
-//           },
-//         method : 'POST'      
-//     };
-
-//   request(options, function (err, res, body) {
-//     if (err) {
-//       console.log('POST ERROR:');
-//       console.log(err.stack);
-//       return;
-//     } else {
-//       console.log('// RETURND //');
-//       console.log(body);
-//       console.log('// ------- //');
-//     }
-//   });
-// }
 
 function postComment (parentId, message) {
-  var text     = message
-    , options  = {
-        url      : 'https://en.reddit.com/api/comment?api_type=json&text=' + encodeURIComponent(text) + '&thing_id=' + parentId,
-        headers  : {
-            'User-Agent' : 'Mongit/0.0.1 by mjkaufer',
-            'X-Modhash'  : modhash,
-            'Cookie'     : 'reddit_session=' + encodeURIComponent(cookie)
-          },
-        method : 'POST'      
-    };
 
-  request(options, function (err, res, body) {
-    if (err) {
-      console.log('COMMENT POST ERROR:');
-      console.log(err.stack);
-      return;
-    } else {
-      console.log('// COMMENT //');
-      console.log(body);
+	if(!loggedIn){//log in and try again
+		return login(function(){
+			postComment(parentId,message);//basically, we call this function again
+		});
+	}
 
-      console.log('// ------- //');
-    }
-  });
+
+	var text = message
+		, options = {
+			url	: 'https://en.reddit.com/api/comment?api_type=json&text=' + encodeURIComponent(text) + '&thing_id=' + parentId,
+			headers	: {
+					'User-Agent' : 'Mongit/0.0.1 by mjkaufer',
+					'X-Modhash'	: modhash,
+					'Cookie' : 'reddit_session=' + encodeURIComponent(cookie)
+				},
+			method : 'POST'			
+		};
+
+	request(options, function (err, res, body) {
+		if (err) {
+			console.log(err.stack);
+			console.log('COMMENT POST ERROR ABOVE!');
+			return;
+		} else {
+			console.log('// COMMENT //');
+			console.log("Your comment was posted successfully!");
+			console.log('// ------- //');
+		}
+	});
 }
 
+function insert(){
+
+}
+
+
 login(function(){
-	postComment("t3_2izhoz","Hurray");
+	console.log("We logged in");
 });
+
+
 
