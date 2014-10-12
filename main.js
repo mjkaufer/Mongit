b5a22c7f0e0671878ce8cf73a77aac8f08968e8f
Matthew Kaufer <matthew@kaufer.org>
2014-10-11 21:05:27 -0400

    REPL and stuff - insert is working!

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 var request	= require('request')
 	, argv	= require('yargs').argv
+	, repl = require('repl')
 	, modhash
 	, cookie;
 
 subredditName = "Mongit";
-
+parentName = "t3_2izkvt";//should be dynamic later, but this for now
 var loggedIn = false;
 
 
@@ -33,7 +34,9 @@ function login (callback) {
 	});
 }
 
-function postComment (parentId, message) {
+function postComment (parentId, message, callback) {
+
+	callback = callback || function(){};
 
 	if(!loggedIn){//log in and try again
 		return login(function(){
@@ -57,23 +60,63 @@ function postComment (parentId, message) {
 		if (err) {
 			console.log(err.stack);
 			console.log('COMMENT POST ERROR ABOVE!');
+			callback(false);
 			return;
 		} else {
-			console.log('// COMMENT //');
-			console.log("Your comment was posted successfully!");
-			console.log('// ------- //');
+			// console.log('// COMMENT //');
+			// console.log("Your comment was posted successfully!");
+			// console.log('// ------- //');
+			callback(true);
 		}
 	});
 }
 
-function insert(){
+function insert(message, parentId, callback){//callback takes one arg, returns true or false whether or not it all worked out
+//we're going to have a static parentId right now - we'll make code to get a list of all parent id's later, but a parentId at the moment is like a table
+	try {
+		message = JSON.parse(JSON.stringify(message));//have to stringify to parse it - weird shtuff
+	} catch (exception) {
+		console.log("Error: " + message + " is not a valid JSON!");
+		console.log(message);
+		return;//we don't want to add to DB
+	}
 
+	parentId = parentId || parentName;
+	callback = callback || function(){
+		console.log("Message " + message + " posted successfully");
+	}
+	postComment(parentId, message, callback);
 }
 
 
+
+//2izkvt
+
 login(function(){
-	console.log("We logged in");
+	console.log("Starting mongit console");
+
+	repl.start({
+		prompt: "Mongit> ",
+		input: process.stdin,
+		output: process.stdout,
+		eval: function(cmd, context, filename, callback){
+			cmd = cmd.substring(1,cmd.length - 2);
+
+
+
+			if(cmd=="exit"){//quit repl if it's "exit"
+				process.exit();
+				console.log("Exited");
+			}
+
+		eval(cmd);
+		console.log("Working");
+
+		}
+	});
 });
+
+
 
 
 
